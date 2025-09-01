32f3d2f15c94066dd59e4e2ec4ec836a4f8d7366
Kushal Das <kushal@sunet.se>
2025-09-01 16:03:22 +0200

    Adds initial /api/v1 API

---
+++ admin/inmoradmin/api.py
@@ -0,0 +1,36 @@
+from django.http import HttpRequest
+from ninja import NinjaAPI, Schema
+from ninja.pagination import LimitOffsetPagination, paginate
+from trustmarks.models import TrustMarkType
+
+api = NinjaAPI()
+
+
+class TrustMarkTypeSchema(Schema):
+    tmtype: str
+    valid_for: int = 365  # How many days the default entry will be valid for
+
+
+@api.post("/trust_mark_type")
+def create_trust_mark_type(request: HttpRequest, data: TrustMarkTypeSchema):
+    """Creates a new trust_mark_type"""
+    try:
+        tmt, created = TrustMarkType.objects.get_or_create(
+            tmtype=data.tmtype, valid_for=data.valid_for
+        )
+        if created:
+            return 200, {"msg": "TrustMarkType created Succesfully.", "id": tmt.id}
+        else:
+            return 403, {"msg": "TrustMarkType already existed.", "id": tmt.id}
+    except Exception as e:
+        print(e)
+        return 500, {"msg": "Error while creating a new TrustMarkType"}
+
+
+@api.get("/trust_mark_type/list", response=list[TrustMarkTypeSchema])
+@paginate(LimitOffsetPagination)
+def list_trust_mark_type(
+    request: HttpRequest,
+):
+    """Lists all existing TrustMarkType(s) from database."""
+    return TrustMarkType.objects.all()
