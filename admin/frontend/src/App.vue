ee3345f68ecd557fed12da32f2d49cdfbf5f440d
Simon <contact@smn.codes>
2025-09-03 18:54:16 +0200

    No need for imports

--- admin/frontend/src/App.vue
+++ admin/frontend/src/App.vue
@@ -1,15 +1,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { RouterView, useRouter, useRoute } from 'vue-router';
 
 export default defineComponent({
   name: 'App',
   mounted() {
-    const route = useRoute();
-    const router = useRouter();
 
-    if (route.fullPath !== 'trustmark-types') {
-      router.push('/trustmark-types')
+    if (this.$route.fullPath !== 'trustmark-types') {
+      this.$router.push('/trustmark-types')
     }
   },
 })
