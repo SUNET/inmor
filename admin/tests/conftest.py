e56627b5c823e89597ac13dbf5e0a918adf85df7
Kushal Das <kushal@sunet.se>
2025-09-04 12:01:21 +0200

    Reformats code for linting

--- admin/tests/conftest.py
+++ admin/tests/conftest.py
@@ -2,13 +2,12 @@ import os
 import sys
 
 import pytest
+from django.core.management import call_command
 from dotenv import load_dotenv
 
 load_dotenv()
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-from django.core.management import call_command
 
 
 @pytest.fixture
