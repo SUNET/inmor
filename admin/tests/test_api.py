efd9ef19f0be6b7ea56b35a9cbaf594c4a0be69f
Kushal Das <kushal@sunet.se>
2025-09-04 15:58:13 +0200

    Updates test for listing trustmarktypes

--- admin/tests/test_api.py
+++ admin/tests/test_api.py
@@ -7,12 +7,13 @@ from inmoradmin.api import router
 def test_trustmarktypes_list(db):
     # don't forget to import router from code above
     self = TestCase()
+    self.maxDiff = None
     trustmark_list = [
-        {"tmtype": "https://sunet.se/does_not_exist_trustmark", "valid_for": 365},
-        {"tmtype": "https://example.com/trust_mark", "valid_for": 365},
+        {"tmtype": "https://sunet.se/does_not_exist_trustmark", "valid_for": 8760, 'active': True, 'autorenew': True,'renewal_time': 48,},
+        {"tmtype": "https://example.com/trust_mark_type", "valid_for": 720, 'active': True, 'autorenew': True,'renewal_time': 48,},
     ]
     client: TestClient = TestClient(router)
-    response = client.get("/trust_mark_type/list")
+    response = client.get("/trustmarktypes")
 
     self.assertEqual(response.status_code, 200)
     marks = response.json()
