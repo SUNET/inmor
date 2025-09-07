2d8fbeb9f0ff6d04386d0721c83c1f03737abf69
Simon <contact@smn.codes>
2025-09-07 13:32:39 +0200

    SDK fetch method error handling

--- admin/frontend/src/lib/admin-sdk/errors.ts
+++ admin/frontend/src/lib/admin-sdk/errors.ts
@@ -1,5 +1,44 @@
 export class AdminSDKError extends Error {};
 
-export class FetchError extends AdminSDKError {};
-
 export class ValidationError extends AdminSDKError {};
+
+
+type FetchErrorProps = {
+    status?: number;
+    message?: string;
+};
+export type FetchErrorType = 'client_error' | 'auth_error' | 'server_error' | 'unknown_error';
+export class FetchError extends AdminSDKError {
+    readonly status?: number;
+    readonly type: FetchErrorType;
+    readonly message: string;
+
+    constructor({ status, message }: FetchErrorProps) {
+        const errors: Record<number, { type: FetchErrorType; message: string }> = {
+            400: { type: 'client_error', message: 'Request is invalid.' },
+            405: { type: 'client_error', message: 'Method not allowed.' },
+            401: { type: 'auth_error',   message: 'Authorization failed.' },
+            403: { type: 'auth_error',   message: 'Access is forbidden.' },
+            500: { type: 'server_error', message: 'Internal server error.' },
+            501: { type: 'server_error', message: 'Requested functionality is not supported.' },
+            502: { type: 'server_error', message: 'Invalid response received from upstream server.' },
+            503: { type: 'server_error', message: 'Service unavailable.' },
+            504: { type: 'server_error', message: 'Gateway timeout: server did not respond in time.' },
+        };
+
+        const error = status && errors[status] || {
+            type: 'unknown_error',
+            message: 'An unexpected error occurred.'
+        };
+
+        if (message) {
+            error.message = message;
+        }
+
+
+        super(`Fetch error: ${error.message}`);
+        this.status = status;
+        this.type = error.type;
+        this.message = error.message;
+    }
+}
--- admin/frontend/src/lib/admin-sdk/sdk.ts
+++ admin/frontend/src/lib/admin-sdk/sdk.ts
@@ -1,13 +1,13 @@
 import * as v from 'valibot';
 import { TrustMarkTypesSchema, type HttpMethod, type TrustMarkTypes } from './resources';
-import { ValidationError } from './errors';
+import { FetchError, ValidationError } from './errors';
 
 type Config = {
     apiUrl: URL;
 };
 
 export class AdminSDK {
-    #apiUrl: URL;
+    readonly #apiUrl: URL;
 
     constructor(config: Config) {
         const { apiUrl } = config;
@@ -28,6 +28,9 @@ export class AdminSDK {
         return data.output;
     }
 
+    /**
+     * @throws {FetchError}
+     */
     async #fetch(method: HttpMethod, path: string|URL, options: RequestInit = {}): Promise<any> {
         const input = new URL(path, this.#apiUrl);
         input.pathname = `/api/v1/${input.pathname.replace(/^\/|\/$/g, '')}`; // Prepend api base path.
@@ -37,17 +40,27 @@ export class AdminSDK {
             method,
             headers: {
                 'Accept': 'application/json',
+                'Content-Type': 'application/json',
                 ...options.headers,
             },
         };
 
-        const res = await fetch(input, init);
-        
-        // ..validation & error handling
+        try {
+            const res = await fetch(input, init);
 
-        const data = await res.json();
+            if (!res.ok) {
+                throw new FetchError({ status: res.status });
+            }
 
-        return data;
+            const data = await res.json();
+            return data;
+        } catch (error) {
+            if (error instanceof FetchError) throw error;
+            throw new FetchError({
+                message: error instanceof Error ? error.message : 'An unexpected error occurred.',
+            });
+        }
+
     }
 }
 
