47a50cea9a7bc58f3c1fb191c5608597ae325a27
Simon <contact@smn.codes>
2025-09-28 18:24:08 +0200

    Add trust mark type creation endpoint & modify list url path

--- admin/frontend/src/lib/admin-sdk/resources.ts
+++ admin/frontend/src/lib/admin-sdk/resources.ts
@@ -2,10 +2,23 @@ import * as v from 'valibot';
 
 export type HttpMethod = 'GET'|'POST'|'DELETE'|'PATCH'|'PUT';
 
+export type TrustMarkTypeCreationOptions = v.InferOutput<typeof TrustMarkTypeCreationOptionsSchema>;
+export const TrustMarkTypeCreationOptionsSchema = v.object({
+  tmtype: v.string(),
+  autorenew: v.boolean(),
+  valid_for: v.number(),
+  renewal_time: v.number(),
+  active: v.boolean(),
+})
+
 export type TrustMarkType = v.InferOutput<typeof TrustMarkTypeSchema>;
 export const TrustMarkTypeSchema = v.object({
+  id: v.number(),
   tmtype: v.string(),
+  autorenew: v.boolean(),
   valid_for: v.number(),
+  renewal_time: v.number(),
+  active: v.boolean(),
 });
 
 export type TrustMarkTypes = v.InferOutput<typeof TrustMarkTypesSchema>;
