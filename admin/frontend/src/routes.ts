108c642ba7651733304588641e4bfd653c99ba39
Simon <contact@smn.codes>
2025-09-29 14:40:21 +0200

    Add remaining views

--- admin/frontend/src/routes.ts
+++ admin/frontend/src/routes.ts
@@ -1,5 +1,9 @@
+import SubordinatesView from "./views/SubordinatesView.vue";
+import TrustMarksView from "./views/TrustMarksView.vue";
 import TrustMarkTypesView from "./views/TrustMarkTypesView.vue";
 
 export const routes = [
     { path: '/trustmark-types', component: TrustMarkTypesView },
+    { path: '/trustmarks', component: TrustMarksView },
+    { path: '/subordinates', component: SubordinatesView },
 ]
---
+++ admin/frontend/src/views/TrustMarksView.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+});
+</script>
+
+<template>
+    <h1>TrustMarks</h1>
+</template>
