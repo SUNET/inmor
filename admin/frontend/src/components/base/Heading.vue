6103c713bfef39894d714ea3f81451d8a6d7b979
Simon <contact@smn.codes>
2025-10-01 13:05:33 +0200

    Add heading component

---
+++ admin/frontend/src/components/base/Heading.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue';
+
+export default defineComponent({
+    name: 'Heading',
+    props: {
+        level: {
+            type: String as PropType<'h1'|'h2'|'h3'|'h4'>,
+            required: true,
+        }
+    }
+});
+</script>
+
+<template>
+    <component :is="level" :class="['heading', `-${level}`]">
+        <slot></slot>
+    </component>
+</template>
+
+<style>
+    .heading {
+
+        &.-h1 {
+            font-size: var(--ir--font-size--l);
+        }
+
+        &.-h2 {
+            font-size: var(--ir--font-size--m);
+        }
+
+        &.-h3 {
+            font-size: var(--ir--font-size--m);
+        }
+
+        &.-h4 {
+            font-size: var(--ir--font-size);
+        }
+    }
+</style>
