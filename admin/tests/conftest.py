9f09eaf5a1728cf0dff049f359cc241410e92c22
Kushal Das <kushal@sunet.se>
2025-11-18 14:41:47 +0100

    chore: refresh db for each test

--- admin/tests/conftest.py
+++ admin/tests/conftest.py
@@ -16,7 +16,7 @@ _ = load_dotenv()
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def db(request, django_db_setup, django_db_blocker):
     django_db_blocker.unblock()
     call_command("loaddata", "db.json")
